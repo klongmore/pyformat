52d70c2b4ae9900188d8356179467bbbbbe61d88
BuildTools <unconfigured@null.spigotmc.org>
2020-01-08 15:00:07 +1100

    Improve readability and PEP 8 compliance

--- pyformat-repo.py
+++ pyformat-repo.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 reponame = sys.argv[1]
-repo = f'https://github.com/klongmore/{reponame}.git'
+repo = f"https://github.com/klongmore/{reponame}.git"
 
 command = f'git clone {repo} && cd {reponame} && python /home/kai/pyformat.py "*.py" && git add . && git commit -m "Improve readability and PEP 8 compliance" && git push origin master && cd .. && sudo rm -rf {reponame}'
 os.system(command)
